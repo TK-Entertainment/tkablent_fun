1deca35c8e4d7ce81ba62e5bfd1230638dfda579
Kevin Chang <52964758+kevin0216@users.noreply.github.com>
2022-10-20 00:33:21 +0800

    Update generator.py

--- modules/generator.py
+++ modules/generator.py
@@ -1,20 +1,22 @@
 import discord
 import random
 import requests
+import json
 
 from .ui.misc import _generate_embed_option
 
 # This will be replaced with static file after testing
-bullshit_database: dict = {}
+bullshit_database: list = []
+bullshit_subject_database: list = ["你媽", "智障", "搜尋紀錄", "詭譎"]
 
 def _generate_random_bullshit(bot, bot_version) -> discord.Embed:
     # if choice == 0, pick from public database api
     # if choice == 1, pick from user-provided database
     choice = random.randint(0, 1)
     if choice == 0 or (choice == 1 and len(bullshit_database) == 0):
-        raw = requests.get('https://api.howtobullshit.me/bullshit')
-        bullshit = raw.text.replace("Bad Request\n", "")
-        bullshit = bullshit.replace("&nbsp;", "")
+        data = {"Topic": random.choice(bullshit_subject_database), "MinLen" : random.randint(10, 60)}
+        raw = requests.post('https://api.howtobullshit.me/bullshit', data=json.dumps(data))
+        bullshit = raw.text.replace("&nbsp;", "")
         bullshit = bullshit.replace("<br>", "\n")
         source = "幹話產生器"
         source_url = "https://howtobullshit.me/"
@@ -30,8 +32,11 @@ def _generate_random_bullshit(bot, bot_version) -> discord.Embed:
 
     return embed
 
-def _generate_meme_embed(bot, bot_version) -> discord.Embed:
-    raw = requests.get('https://memes.tw/wtf/api')
+def _generate_meme_embed(bot, bot_version, contests: int=None) -> discord.Embed:
+    if contests is not None:
+        raw = requests.get(f'https://memes.tw/wtf/api?contest={contests}')
+    else:
+        raw = requests.get(f'https://memes.tw/wtf/api')
     recommendation = raw.json()
 
     random_recommendation = random.choice(recommendation)
